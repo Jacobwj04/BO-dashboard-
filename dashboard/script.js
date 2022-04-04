692bef8dd616d9d6ee5bf4c5cdc75d5837209465
Jacobwj04 <32894@ma-web.nl>
2022-04-04 09:18:44 +0200

    new layout

--- dashboard/script.js
+++ dashboard/script.js
@@ -1,34 +1,30 @@
 var ctx = document.getElementById('myChart');
 
-var stars = [4.15, 3.78, 3.68, 4.15, 4.30];
+var energie = [4.15, 3.78, 3.68, 4.15, 4.30];
 var frameworks = ['Maandag', 'Dinsdag', 'Woensdag', 'Donderdag', 'Vrijdag'];
 
+Chart.defaults.font.size = 16;
+Chart.defaults.color = 'black';
 var myChart = new Chart(ctx, {
     type: 'line',     
     data: { 
         labels: frameworks,
         datasets: [{ 
             label: 'Energie verbruik',
-            data: stars,
+            data: energie,
             backgroundColor: 
-            ["rgba(255, 99, 132, 0.2)",
-             "rgba(54, 162, 235, 0.2)",
-             "rgba(255, 206, 86, 0.2)",
-             "rgba(75, 192, 192, 0.2)",
-             "rgba(153, 102, 255, 0.2)" 
+            ["rgb(0,0,0);", 
             ],
             borderColor: 
-            [  "rgba(255, 99, 132, 1)",
-             "rgba(54, 162, 235, 1)",
-              "rgba(255, 206, 86, 1)",
-               "rgba(75, 192, 192, 1)",
-               "rgba(153, 102, 255, 1)", 
+            [  "rgba(0,0,0)",
             ],
             borderWidth: 1,
             borderRadius: 15, 
-        }]  
+        }]   
     }, 
-}) 
+})
+
+
 
 
 var today = new Date();
